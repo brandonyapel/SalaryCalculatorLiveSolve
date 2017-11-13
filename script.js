d47eb40350145804e361227d6d7ec31fcb6a7b9d
Brandon Yapel <brandonyapel@gmail.com>
2017-11-13 10:29:35 -0600

    step 3

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function readyNow () {
     console.log('JQ');
     $("#addEmployeeButton").on('click',addEmployeeButtonClick)
 }//end doc reay
+var employees = []
 
 function addEmployeeButtonClick () {
     console.log('in addEmployeeButtonClick');
@@ -19,9 +20,28 @@ function addEmployeeButtonClick () {
     }
     console.log(newEmployee)
     //push the new object into an array
-    var employees = []
+    
     employees.push(newEmployee);
     console.log(employees);
-    
-    
+    calculateSalaries();
 }//end addEmployeeButtonClick
+
+function calculateSalaries () {
+    console.log( 'in calculateSalaries');
+    var totalSalaries = 0;
+    //loop through employees array
+    for (let i = 0; i < employees.length; i++) {
+        console.log( employees[ i ] );
+        //convert each salary to a number
+        console.log(employees[i].salary)
+        //add salary to total salaries
+        totalSalaries += Number( employees[i].salary);
+    }//end for each employee
+    console.log('total salaries:', totalSalaries)
+    var monthlySalaryCost = totalSalaries/12;
+    console.log('monthly cost', monthlySalaryCost)
+    //convert eacj salary to a number
+
+    //add salary to total salaries
+
+};//end calculateSalaries monthlyS
